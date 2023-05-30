09d79f63f94eba3e4cd8a1228dc7fb2c99cfd303
Stefan Bogdanovic <sbogdanovic@nanointeractive.com>
2023-05-30 14:12:25 +0200

    Logging finished
    Signed-off-by: Stefan Bogdanovic <sbogdanovic@nanointeractive.com>

---
+++ src/RetryPolicy/Logging.stub.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* @generate-class-entries
+*/
+namespace Cassandra\RetryPolicy {
+    /**
+     * @strict-properties
+     */
+    final class Logging implements \Cassandra\RetryPolicy
+    {
+        public function __construct(\Cassandra\RetryPolicy $policy) {}
+    }
+}
