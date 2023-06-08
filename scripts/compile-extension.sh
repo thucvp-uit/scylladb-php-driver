f0660c757be956951e0ca0d2f8ddda8de0055d47
Dusan Malusev <dusan@dusanmalusev.dev>
2023-06-09 01:25:12 +0200

    php binary changed
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- scripts/compile-extension.sh
+++ scripts/compile-extension.sh
@@ -9,6 +9,6 @@ ninja install || exit 1
 
 popd || exit 1
 
-PHP_INI=$("$HOME/php/bin/php" --ini | grep "php.ini" | awk '{ print $5 }' | xargs -I {} printf '%s/php.ini' {})
+PHP_INI=$("/root/php/bin/php" --ini | grep "php.ini" | awk '{ print $5 }' | xargs -I {} printf '%s/php.ini' {})
 
 cat cassandra.ini >>"$PHP_INI" || exit 1
