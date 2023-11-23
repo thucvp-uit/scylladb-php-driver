fe2a72c09896d696d08ee0b438aa5cad1877faf3
Dusan Malusev <dusan@dusanmalusev.dev>
2023-11-23 21:37:01 +0100

    Remove target folder
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- target/debug/deps/jobserver-9d26716be9619087.d
+++
@@ -1,9 +0,0 @@
-/home/dmalusev/Code/scylladb-php-driver/target/debug/deps/libjobserver-9d26716be9619087.rmeta: /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/lib.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/error.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/unix.rs
-
-/home/dmalusev/Code/scylladb-php-driver/target/debug/deps/libjobserver-9d26716be9619087.rlib: /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/lib.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/error.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/unix.rs
-
-/home/dmalusev/Code/scylladb-php-driver/target/debug/deps/jobserver-9d26716be9619087.d: /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/lib.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/error.rs /home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/unix.rs
-
-/home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/lib.rs:
-/home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/error.rs:
-/home/dmalusev/.cargo/registry/src/index.crates.io-6f17d22bba15001f/jobserver-0.1.27/src/unix.rs:
